fe76ea5a07b887fc3550fa6d49ea3c4396f300f5
Devin-Springer <devinspringersemail@gmail.com>
2017-03-31 13:39:44 -0400

    Update Main.java

--- discordbot/src/main/java/vulpecidic/discordbot/Main.java
+++ discordbot/src/main/java/vulpecidic/discordbot/Main.java
@@ -5,7 +5,7 @@ import de.btobastian.javacord.Javacord;
 
 public class Main {
 	
-	private static String token = "MjgzODI4NTY3MjUzNTgxODI0.C46vuA.z9E_pmaso-PRHoolQpgaMiutOLk";
+	private static String token = "";
 
 	public static void main(String[] args) {
 		DiscordAPI api = Javacord.getApi(token, true);
